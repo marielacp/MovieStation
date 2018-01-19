b8af49924cd25ad2383c8fc13e9595821dd299fb
Claudiagari <claudia.garibotto@gmail.com>
2018-01-19 05:56:55 -0500

    mostrando pelicula y detalles

--- js/home.js
+++ js/home.js
@@ -9,9 +9,32 @@ $(document).ready(function() {
         $('.perfil').attr('src', '../assets/img/user.png');
       } else {
         $('.perfil').attr('src', photoURL);
-      }     
-
-
+      };
+      $('#searchForm').on('submit', (e) => {
+        var searchText = $('#searchText').val();
+        getMovies(searchText);
+        e.preventDefault();
+      });
+      function getMovies(searchText) {
+        axios.get('http://www.omdbapi.com/?s=' + searchText + '&apikey=69504f73')
+          .then((response) => {
+            console.log(response);
+            var movies = response.data.Search;
+            var output = '';
+            $.each(movies, (index, movie) => {
+              var pelis = ('<div class="col-md-3"><div class="well text-center"><img src=\'' + movies[index].Poster + '\' class="img-responsive"><h5>' + movies[index].Title + '</h5><a id=' + movies[index].imdbID + ' class="btn btn-primary" href="#">Movie Details</a></div></div>');
+              $('#movies').prepend(pelis); 
+             
+              $('#' + movies[index].imdbID + '').on('click', function() {
+                localStorage.codeNum = movies[index].imdbID;
+                $(location).attr('href', 'movie.html');
+              });
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     } else {
       // sign out
     }
---
+++ js/movie.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+  var database = firebase.database();
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log('existe usuario activo');
+      var displayName = user.displayName;
+      var photoURL = user.photoURL;
+      var uid = user.uid;
+      function getMovie() {
+        var movieId = localStorage.getItem('codeNum');
+        console.log(movieId);
+      
+        axios.get('http://www.omdbapi.com?i=' + movieId + '&apikey=69504f73')
+          .then((response) => {
+            console.log(response);
+            let movie = response.data;
+      
+            let output = `
+              <div class="row">
+                <div class="col-md-4">
+                  <img src="${movie.Poster}" class="thumbnail">
+                </div>
+                <div class="col-md-8">
+                  <h2>${movie.Title}</h2>
+                  <ul class="list-group">
+                    <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                    <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                    <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                    <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                    <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                    <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                    <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="row">
+                <div class="well">
+                  <h3>Plot</h3>
+                  ${movie.Plot}
+                  <hr>
+                  <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+                  <a href="home.html" class="btn btn-default">Go Back To Search</a>
+                </div>
+              </div>
+            `;
+      
+            $('#movie').html(output);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      getMovie()
+    } else {
+
+    }
+  });
+});
